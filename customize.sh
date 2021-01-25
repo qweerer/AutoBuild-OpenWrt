20641b1d2052cad452d3f02e73c2f85398b54a6d
Tiening <liu2946181970@126.com>
2021-01-25 08:44:43 +0800

    名字有问题，这个是增加kenzok8依赖的

--- customize.sh
+++ customize.sh
@@ -11,4 +11,4 @@ sed -i 's/192.168.1.1/192.168.2.2/g' openwrt/package/base-files/files/bin/config
 # 自定义 sh
 device_name='X86-DOCK'
 echo "修改机器名称"
-sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt/$device_name/g" openwrt/package/base-files/files/bin/config_generate
