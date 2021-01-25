96a5f97cecdafb5b01a93e86e0acbb80ffbe56d0
Tiening <liu2946181970@126.com>
2021-01-25 08:33:18 +0800

    可能是依赖的问题，删除看下

--- customize.sh
+++ customize.sh
@@ -7,3 +7,8 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' openwrt/package/base-files/files/bin/config_generate
+
+# 自定义 sh
+device_name='X86-DOCK'
+echo "修改机器名称"
+sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
